c985aed553122f60090ee503b6d6fec62a69ca43
blindmansion <blindmansion@gmail.com>
2026-03-20 15:07:07 -0400

    bump version

--- src/git.ts
+++ src/git.ts
@@ -12,7 +12,7 @@ import {
 } from "./hooks.ts";
 import type { ObjectStore, RefStore, RemoteResolver } from "./lib/types.ts";
 
-export const VERSION = "1.2.11";
+export const VERSION = "1.2.12";
 
 /** Options for subcommand execution (mirrors just-bash's CommandExecOptions). */
 export interface CommandExecOptions {
